7c0d3356d86fdf439c122269482bfcd44c594b82
Korzeremi02 <remi.k@cashflowpositif.fr>
2025-05-26 14:17:11 +0200

    ci: navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [NavigationBar].
-
 void main() => runApp(const NavigationBarApp());
 
 class NavigationBarApp extends StatelessWidget {
@@ -22,13 +20,12 @@ class NavigationExample extends StatefulWidget {
 
 class _NavigationExampleState extends State<NavigationExample> {
   int currentPageIndex = 0;
-  NavigationDestinationLabelBehavior labelBehavior = NavigationDestinationLabelBehavior.alwaysShow;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: NavigationBar(
-        labelBehavior: labelBehavior,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
         selectedIndex: currentPageIndex,
         onDestinationSelected: (int index) {
           setState(() {
@@ -36,53 +33,14 @@ class _NavigationExampleState extends State<NavigationExample> {
           });
         },
         destinations: const <Widget>[
-          NavigationDestination(icon: Icon(Icons.explore), label: 'Explore'),
-          NavigationDestination(icon: Icon(Icons.commute), label: 'Commute'),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.bookmark),
-            icon: Icon(Icons.bookmark_border),
-            label: 'Saved',
-          ),
+          NavigationDestination(icon: Icon(Icons.explore), label: 'Explorer'),
+          NavigationDestination(icon: Icon(Icons.book), label: 'Mes réservations'),
+          NavigationDestination(icon: Icon(Icons.face), label: 'Mon profil'),
         ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Label behavior: ${labelBehavior.name}'),
-            const SizedBox(height: 10),
-            OverflowBar(
-              spacing: 10.0,
-              overflowAlignment: OverflowBarAlignment.center,
-              overflowSpacing: 10.0,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      labelBehavior = NavigationDestinationLabelBehavior.alwaysShow;
-                    });
-                  },
-                  child: const Text('alwaysShow'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      labelBehavior = NavigationDestinationLabelBehavior.onlyShowSelected;
-                    });
-                  },
-                  child: const Text('onlyShowSelected'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      labelBehavior = NavigationDestinationLabelBehavior.alwaysHide;
-                    });
-                  },
-                  child: const Text('alwaysHide'),
-                ),
-              ],
-            ),
-          ],
         ),
       ),
     );
