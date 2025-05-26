9e202d97322706581543eedc7df7c0726d54c778
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2025-05-26 15:53:17 +0200

    feat: exploser

--- lib/main.dart
+++ lib/main.dart
@@ -89,6 +89,8 @@ class ExploreTab extends StatefulWidget {
 }
 
 class _ExploreTabState extends State<ExploreTab> {
+  DateTime? selectedDate;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -108,13 +110,28 @@ class _ExploreTabState extends State<ExploreTab> {
               ),
               child: Column(
                 children: [
-                  Text(
-                    'Choisir date',
-                    style: TextStyle(fontSize: 10),
-                    textAlign: TextAlign.left,
+                  TextButton(
+                    onPressed: () async {
+                      final DateTime? picked = await showDatePicker(
+                        context: context,
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime.now().add(Duration(days: 365)),
+                        initialDate: DateTime.now(),
+                      );
+                      if (picked != null) {
+                        setState(() {
+                          selectedDate = picked;
+                        });
+                      }
+                    },
+                    child: Text(
+                      'Choisir date',
+                      style: TextStyle(fontSize: 10),
+                      textAlign: TextAlign.left,
+                    ),
                   ),
                   Text(
-                    '26/05/2025',
+                    selectedDate?.toString() ?? 'Aucune date sélectionnée',
                     style: TextStyle(fontSize: 14),
                     textAlign: TextAlign.left,
                   ),
@@ -123,12 +140,206 @@ class _ExploreTabState extends State<ExploreTab> {
             ),
             Container(
               width: (MediaQuery.of(context).size.width) / 2,
-              padding: EdgeInsets.fromLTRB(4.0, 4.0, 16.0, 4.0),
+              padding: EdgeInsets.fromLTRB(4.0, 14.0, 16.0, 4.0),
+
               child: Column(
+                spacing: 10,
                 children: [
-                  Text('Nombre de chambres', style: TextStyle(fontSize: 10)),
+                  Text('Nombre de chambres', style: TextStyle(fontSize: 10),),
                   Text('1 chambre - 2 adultes', style: TextStyle(fontSize: 14)),
                 ],
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              width: (MediaQuery.of(context).size.width),
+              height: 300,
+              child: Card(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 34,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Hotel 4* Bali',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          child: Image.asset(
+                            'assets/hotel1.jpg',
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 25,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Chambre - 2 adultes',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              height: 25,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'A partir du 30/05/2025',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 25,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '1162€ pour 7 nuits',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+              width: (MediaQuery.of(context).size.width),
+              height: 300,
+              child: Card(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 34,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Hotel 4* Bali',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          child: Image.asset(
+                            'assets/hotel1.jpg',
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 25,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Chambre - 2 adultes',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              height: 25,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'A partir du 30/05/2025',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 25,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '1162€ pour 7 nuits',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
